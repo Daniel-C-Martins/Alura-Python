f1159df509a223cebc3a0e4205992ccc6c63c686
Daniel-C-Martins <danicoxa.martins@outlook.com>
2024-01-15 13:04:49 -0300

    fim_curso_2

--- Curso2/Modelos/restaurantes.py
+++ Curso2/Modelos/restaurantes.py
@@ -1,40 +1,42 @@
 from Modelos.avaliacao import Avaliacao
 
 class Restaurante:
-  restaurantes = []
-  def __init__(self, nome, categoria): ##Construtor / Self == This
-      self._nome = nome.title()
-      self._categoria = categoria
-      self._ativo = False                   ##_ativo similar ao private
-      self._avaliacao = []
-      Restaurante.restaurantes.append(self) ##Adicionando restaurante a lista sempre que criar
+    """Representa um restaurante e suas características."""
+    restaurantes = []
 
-  def __str__(self):  ##ToString
-      return f"{self._nome} | {self._categoria}"
+    def __init__(self, nome, categoria): ##Construtor / Self == This
+        self._nome = nome.title()
+        self._categoria = categoria
+        self._ativo = False                   ##_ativo similar ao private
+        self._avaliacao = []
+        Restaurante.restaurantes.append(self) ##Adicionando restaurante a lista sempre que criar
 
-  @classmethod
-  def listar_restaurante(cls):
-      print(f'{"Nome do restaurante".ljust(25)} | {"Categoria do restaurante".ljust(25)} | {"Avaliação do restaurante".ljust(25)} | {"Status do restaurante"}')
-    
-      for restaurante in cls.restaurantes:
-          print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.media_avaliacoes().ljust(25)}| {restaurante._ativo}")
+    def __str__(self):  ##ToString
+        return f"{self._nome} | {self._categoria}"
 
-  @property
-  def ativo(self):        ##Modificando o _ativo
-      return "✅" if self._ativo else "❌"
+    @classmethod
+    def listar_restaurantes(cls):
+            print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Avaliação".ljust(25)} |{"Status"}')
+            for restaurante in cls.restaurantes:
+                print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
 
-  def alternar_estado(self):
-      self._ativo = not self._ativo
+    @property
+    def ativo(self):        ##Modificando o _ativo
+        return "✅" if self._ativo else "❌"
 
-  def receber_avaliacao(self, cliente, nota):
-      avaliacao = Avaliacao(cliente, nota)
-      self._avaliacao.append(avaliacao) 
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-  @property
-  def media_avaliacoes(self):
-      if not self._avaliacao:
-          return 0
-      soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao) ##Faz a soma de todas notas de avaliação
-      quantidade_de_notas = len(self._avaliacao)
-      media = round(soma_das_notas / quantidade_de_notas, 1) ##Round arredonda
-      return media
+    def receber_avaliacao(self, cliente, nota):
+        if nota > 0 and nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao) 
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return "."
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao) ##Faz a soma de todas notas de avaliação
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1) ##Round arredonda
+        return media
--- Curso2/app.py
+++ Curso2/app.py
@@ -4,12 +4,13 @@ restaurante_praca = Restaurante("Praca", "Gourmet")
 # restaurante_mexicano = Restaurante("Mexican food", "Mexicano")
 # restaurante_pizza = Restaurante("Japa", "Japonês")
 
+
 restaurante_praca.receber_avaliacao("Gui", 10)
 restaurante_praca.receber_avaliacao("Giovanan", 8)
 restaurante_praca.receber_avaliacao("Carlos", 5)
 
 def main():
-    Restaurante.listar_restaurante()
+    Restaurante.listar_restaurantes()
 
 if __name__ == "__main__":
     main()
